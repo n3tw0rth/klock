94204be0c670b6f4fb22c2b51c9b0c1a55e55e85
n3tw0rth <40062006+n3tw0rth@users.noreply.github.com>
2025-04-25 22:07:24 +0530

    wip: clockify clock

--- src/clocks/jira.rs
+++ src/clocks/jira.rs
@@ -1,13 +1,2 @@
-use super::Clock;
-use crate::error::Result;
 
-struct JiraClock {}
 
-impl Clock for JiraClock {
-    async fn new() -> Self {
-        unimplemented!()
-    }
-    async fn log() -> Result<()> {
-        unimplemented!()
-    }
-}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use jired::clocks::{Clock, clockify::ClockifyClock};
 use jired::tracing::Tracer;
 use jired::{Args, boards::Board, boards::jira::Jira, error::Result};
 
@@ -7,7 +8,9 @@ async fn main() -> Result<()> {
     Tracer::init()?;
     let args = Args::parse();
 
+    // commented to test the clockify
     Jira::new().await.init(args).await?;
 
+    ClockifyClock::new().await.init().await?;
     Ok(())
 }
